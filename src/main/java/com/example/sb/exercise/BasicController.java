3f6c19c0be6012464826cd2ba5e5c3f253eb9fa5
GimGiju <tigerwonder@naver.com>
2024-03-06 09:49:06 +0900

    스프링부트 2일차 1교시

--- src/main/java/com/example/sb/exercise/BasicController.java
+++ src/main/java/com/example/sb/exercise/BasicController.java
@@ -1,8 +1,10 @@
 package com.example.sb.exercise;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/ex")
@@ -10,9 +12,23 @@ public class BasicController {
 
 	@GetMapping("/hello")	// localhost:8090/sb/ex/hello
 	public String hello() {
-		return "exercise/hello"; 		// hello.html를 의미
+		return "exercise/hello"; 		// hello.html를 의미	// 파일이름
 	}
 	
+	@ResponseBody			// html 파일을 찾지 말고, 데이터를 직접 전송하라는 코드
+	@GetMapping("/noHTML")
+	public String noHTML() {
+		return "<h1>Hello Spring Boot!!!</h1>";
+	}
 	
+	@GetMapping("/redirect")
+	public String redirect() {
+		return "redirect:/ex/hello";		// Redirection 가능 hello로 보내줌
+	}
 	
+	@GetMapping("/params")
+    public String params(Model model){
+        model.addAttribute("name","제임스");
+        return "exercise/params";
+    }
 }
