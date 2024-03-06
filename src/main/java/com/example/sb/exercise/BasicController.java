d5e387b8eb67bfa45e1f19a0549f9c42ee63d7a6
GimGiju <tigerwonder@naver.com>
2024-03-06 10:49:15 +0900

    스프링부트 2일차 2

--- src/main/java/com/example/sb/exercise/BasicController.java
+++ src/main/java/com/example/sb/exercise/BasicController.java
@@ -1,15 +1,22 @@
 package com.example.sb.exercise;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/ex")
 public class BasicController {
-
+	private final Logger log = LoggerFactory.getLogger(getClass());
+	
 	@GetMapping("/hello")	// localhost:8090/sb/ex/hello
 	public String hello() {
 		return "exercise/hello"; 		// hello.html를 의미	// 파일이름
@@ -31,4 +38,29 @@ public class BasicController {
         model.addAttribute("name","제임스");
         return "exercise/params";
     }
+	
+	@GetMapping("/params2")
+    public String params2(Model model, HttpServletRequest req){				// jakarta.servlet.http.HttpServletRequest
+		String name = req.getParameter("name"); 
+        model.addAttribute("name", name);
+        return "exercise/params";
+    }
+	
+	@GetMapping("/params3")				// 바로 위 코드와 동일하게 작동가능
+    public String params3(Model model, String name, int count){				
+        model.addAttribute("name", name + count);
+        return "exercise/params";
+    }
+	
+	@GetMapping("/memberForm")
+	public String memberForm() {
+		return "exercise/memberForm";
+	}
+	
+	@PostMapping("/memberProc")
+	public String memberProc(Member member, Model model) {
+		log.info(member.toString());
+		model.addAttribute("name", member.getName());
+		return  "exercise/params";
+	}
 }
